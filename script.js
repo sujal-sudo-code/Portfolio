321fa143b36788cd7e5f1f6e48f9ab89b4cdc1a1
Sujal Rathore <work.sujalrathore@gmail.com>
2024-10-21 18:46:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -14,16 +14,7 @@ document.addEventListener('click', function(event) {
     }
 });
 
-window.addEventListener('scroll', function() {
-    const fadeSections = document.querySelectorAll('.fade-section');
-    const scrollPosition = window.pageYOffset || document.documentElement.scrollTop;
 
-    fadeSections.forEach(section => {
-        const sectionPosition = section.getBoundingClientRect().top + window.scrollY;
-        const maxOpacityScroll = 200; /
-        const opacity = Math.min(1, (scrollPosition + window.innerHeight - sectionPosition) / maxOpacityScroll);
-
-        if (opacity > 0) {
             section.style.opacity = opacity;
             section.style.transform = `scale(${0.8 + opacity * 0.2})`; 
         }
