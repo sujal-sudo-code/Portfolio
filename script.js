71345a71b93bb8a9ee495e33cacbba5116922e8e
Sujal Rathore <work.sujalrathore@gmail.com>
2024-11-29 17:50:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,30 @@
-document.addEventListener('click', function(event) {
+document.addEventListener('click', function (event) {
     const clickedImage = event.target.closest('.clickable');
 
     if (clickedImage) {
-        document.querySelectorAll('.clickable').forEach(img => {
-            img.classList.remove('enlarged');
-        });
-
-        clickedImage.classList.add('enlarged');
-    } else {
-        document.querySelectorAll('.clickable').forEach(img => {
-            img.classList.remove('enlarged');
-        });
+        if (clickedImage.classList.contains('enlarged')) {
+            clickedImage.classList.remove('enlarged');
+        } else {
+            document.querySelectorAll('.clickable').forEach(img => {
+                img.classList.remove('enlarged');
+            });
+            clickedImage.classList.add('enlarged');
+        }
     }
 });
 
+let slideIndex = 0;
+showSlides();
 
-            section.style.opacity = opacity;
-            section.style.transform = `scale(${0.8 + opacity * 0.2})`; 
-        }
-    });
-});
+function showSlides() {
+    let slides = document.getElementsByClassName("slide");
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none"; 
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) { 
+        slideIndex = 1;
+    }
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(showSlides, 3000);
+}
